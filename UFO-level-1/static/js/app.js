89c2eb9a6af92e786d41eaecb28671f263f3b593
Brent Meulebroeck <bmeulebroeck@gmail.com>
2021-01-02 16:40:32 -0600

    Level 1 working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,48 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Code to display the data in the table
+var tbody = d3.select("tbody");
+
+data.forEach(function(UFOReport) {
+    var row = tbody.append("tr");
+
+    Object.entries(UFOReport).forEach(function([key, value]) {
+        row.append("td").text(value);
+    });
+});
+
+// Filter by date
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+
+    // Select the input element and store the date value entered
+    var inputElement = d3.select("#datetime");
+    var inputDate = inputElement.property("value");
+
+    console.log(inputDate);
+
+    // Filter the data by the entered date and log to console to verify it's working
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputDate);
+
+    console.log(filteredData);
+
+    // Clear the table
+    tbody.html("");
+
+    // Loop through the filtered data and add it into the table
+    filteredData.forEach(function(filterDate) {
+        var row = tbody.append("tr");
+
+        Object.entries(filterDate).forEach(function([key, value]) {
+            row.append("td").text(value);
+        })
+    })
+}
